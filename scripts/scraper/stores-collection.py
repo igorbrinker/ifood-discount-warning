b1f67e842c9cfbb3a1fd7b949796a7e0ae2bccb9
igorgti <igorbrinker@gmail.com>
2022-05-30 17:15:38 -0300

    🎰Stores count added

--- scripts/scraper/stores-collection.py
+++ scripts/scraper/stores-collection.py
@@ -10,9 +10,13 @@ def restaurants():
     bfs = BeautifulSoup(page.content, 'html.parser')
     lists = bfs.find_all('li', class_='restaurants-list__item-wrapper')
 
+    i = 0
+
     for list in lists:
         name = list.find('span', class_='restaurant-name').text.replace('\n', '')
-        
+        i = i + 1
         print(name)
+
+    print('Restaurant count:', i)    
 
 restaurants()
