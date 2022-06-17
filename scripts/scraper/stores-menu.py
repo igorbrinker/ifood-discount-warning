8f2d62e3c7131daa3928e46de953d9d4dff97be7
igorgti <igorbrinker@gmail.com>
2022-06-17 15:44:12 -0300

    😈Now using selenium to mock access to the website and scrap restaurant's menu items

--- scripts/scraper/stores-menu.py
+++ scripts/scraper/stores-menu.py
@@ -2,45 +2,58 @@
 from genericpath import exists
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 import json
 import requests
 import re
 
-drive = webdriver.Chrome(executable_path=r'../../src/media/chromedriver-102.exe')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 def menu():
     with open('../../src/data/stores-collection/stores.json', 'r', encoding='utf8') as input:
        input_data = json.load(input)
 
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(chrome_options=options)
-    # i = 0
+    # options = Options()
+    # options.add_argument('--headless')
+    # options.add_argument('--disable-gpu')
+
+    i = 0
 
     output = []
 
-    # while i < (len(input_data)):
-        # print(i)
+    while i < (len(input_data)):
         #getting all the urls saved until the last id
-    target_url = input_data[1]['url']
-    driver.get(target_url)
-    time.sleep(3)
-    page = driver.page_source
+        target_url = input_data[i]['url']
+        driver.get(target_url)
+        time.sleep(10)
+        page = driver.page_source
+        bfs = BeautifulSoup(page)
+        lists = bfs.find_all('main', class_='main-layout')
+
+        for list in lists:
+            try:
+                restaurant_name = list.find('h1', class_='merchant-info__title').text
+                # print(restaurant_name)
+                time.sleep(20)
+                i+= 1
+
+            except AttributeError:
+                restaurant_name = list.find('h1', class_='market-header__title').text
+                # print(restaurant_name)
+                time.sleep(20)
+                i+= 1
+            
+            data = {
+                "restaurant_name": restaurant_name
+            }
+
+            output.append(data)
+
+    with open('../../src/data/stores-menu/stores-menu.json', 'a+', encoding='utf8') as file:
+        json.dump(output, file, ensure_ascii = False, indent = 4, sort_keys = True)
+
     driver.quit()
-    bfs = BeautifulSoup(page.content, 'html.parser')
-    lists = bfs.find_all('div')
-
-    print(target_url, '\n\n\n\n\n\n\n\n')
-    print(lists, '\n')
-
-        # for list in lists:
-        #     name = list.find('h1', class_='merchant-info__title').text.replace('\n', ' ')
-        #     print(name)
-
-        # i+= 1
-
         
 menu()
