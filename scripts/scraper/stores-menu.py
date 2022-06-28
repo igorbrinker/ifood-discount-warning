bb9e6d02659091be3ce9aa4ae192c251fe87eb13
igorgti <igorbrinker@gmail.com>
2022-06-28 10:45:33 -0300

    🎃 Errors fallback added

--- scripts/scraper/stores-menu.py
+++ scripts/scraper/stores-menu.py
@@ -24,6 +24,7 @@ def menu():
     output = []
 
     while i < (len(input_data)):
+    # while i < 1:
         #getting all the urls saved until the last id
         target_url = input_data[i]['store_url']
         store_id = input_data[i]['store_id']
@@ -40,33 +41,57 @@ def menu():
 
             try:
                 store_name = list.find('h1', class_='merchant-info__title').text
-                menu = list.find_all('div', class_='restaurant-column')
+                fast_menu = list.find_all('div', class_='restaurant__fast-menu')
+                time.sleep(3)
+
+                print(store_name)
+
+                for menus in fast_menu:
+                    menu = menus.find_all('div', class_='restaurant-menu-group')
+                    print(len(menu))
+                    time.sleep(3)
                 
-                for item in menu:
-                    product_name = item.find('h3', class_='dish-card__description').text
-                    product_description = item.find('span', class_='dish-card__details').text
-                    product_price = item.find('span', class_='dish-card__price').text 
+                    for item in menu:
+                        menu_sections = item.find_all('div', class_='dish-card-wrapper')
+                        for item_section in menu_sections:
+                            try:
+                                product_name = item_section.find('h3', class_='dish-card__description').text
+                            except AttributeError:
+                                product_name = ''
+                            try:
+                                product_description = item_section.find('span', class_='dish-card__details').text
+                            except AttributeError:
+                                product_description = ''
+                            try:
+                                product_price = item_section.find('span', class_='dish-card__price').text
+                            except AttributeError:
+                                product_price = ''
 
-                    product = {
-                        "product_id": product_id,
-                        "product_name": product_name,
-                        "product_description": product_description, 
-                        "product_price":product_price
-                    }
+                            product = {
+                                "product_id": product_id,
+                                "product_name": product_name,
+                                "product_description": product_description , 
+                                "product_price":product_price
+                            }
 
-                    products.append(product)
-                    
-                    product_id+= 1
+                            products.append(product)
+                            
+                            product_id+= 1
+                
+                print(products)
 
-                    time.sleep(5)
+                time.sleep(5)
                     
                 i+= 1
 
             except AttributeError:
-                store_name = list.find('h1', class_='market-header__title').text
+                try:
+                    store_name = list.find('h1', class_='market-header__title').text
+                except AttributeError:
+                    store_name = ''
 
                 # print(restaurant_name)
-                time.sleep(5)
+                # time.sleep(5)
                 i+= 1
 
             data = {
